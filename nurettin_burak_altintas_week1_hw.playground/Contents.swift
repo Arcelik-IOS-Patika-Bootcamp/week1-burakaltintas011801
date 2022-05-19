0651eaff559ac7bf31dac965f8d0b54bd26a3960
burakaltintas011801 <burak.altintas@ozu.edu.tr>
2022-05-19 16:08:47 +0300

    Garden class and Flower struct has been created.

--- nurettin_burak_altintas_week1_hw.playground/Contents.swift
+++ nurettin_burak_altintas_week1_hw.playground/Contents.swift
@@ -1,3 +1,29 @@
 import UIKit
 
-var greeting = "Hello, playground"
+
+
+
+class Garden
+{
+    var owner : String
+    var flowers : [Flower]
+    
+    init(_ owner : String, _ flowers: [Flower]) {
+        self.owner = owner
+        self.flowers = flowers
+    }
+    
+    func waterYourGarden()
+    {
+            print("Your garden is watering...")
+    }
+    
+}
+
+struct Flower {
+    var name : String
+    var familia : String
+    var age : String
+    var color : String
+    
+}
